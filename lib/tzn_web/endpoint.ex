cab30371ecbe263652fafc957a2b1f9d342c99f9
Dharik Patel <dharik@magoosh.com>
2020-09-20 16:22:48 -0400

    Do a persistent session thing

--- lib/tzn_web/endpoint.ex
+++ lib/tzn_web/endpoint.ex
@@ -51,5 +51,6 @@ defmodule TznWeb.Endpoint do
   plug Plug.Head
   plug Plug.Session, @session_options
   plug Pow.Plug.Session, otp_app: :tzn
+  plug PowPersistentSession.Plug.Cookie
   plug TznWeb.Router
 end
