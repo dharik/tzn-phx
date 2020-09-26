95c4358f2cb7de03a285e9ad2e81134097c8c5f9
Dharik Patel <dharik@magoosh.com>
2020-09-26 19:01:17 -0400

    Send strategy session email to both parents

--- lib/tzn/jobs/send_strategy_session_emails.ex
+++ lib/tzn/jobs/send_strategy_session_emails.ex
@@ -4,13 +4,12 @@ defmodule Tzn.Jobs.SendStrategySessionEmails do
   alias Tzn.Transizion.StrategySession
   alias Tzn.Repo
 
-  @auth "FILL ME"
+  @auth ""
+  @template_id = ""
+  @sendgrid_url = "https://api.sendgrid.com/v3/mail/send"
+
 
   def run do
-    # From sendgrid
-    template_id = "d-6b8a7733b8fb4a4a957fcb9a24f9f61a"
-    url = "https://api.sendgrid.com/v3/mail/send"
-
     headers = [
       Authorization: @auth,
       "Content-Type": "application/json"
@@ -24,46 +23,77 @@ defmodule Tzn.Jobs.SendStrategySessionEmails do
       |> Repo.preload([:mentee, :mentor])
 
     sessions
-    |> Enum.filter(fn s ->
-      # TODO: Parent2 email case
-      s.mentee.parent1_email
-    end)
     |> Enum.map(fn s ->
-      body = %{
-        personalizations: [
-          %{
-            to: [
-              %{
-                email: s.mentee.parent1_email,
-                name: s.mentee.parent1_name
-              }
-            ],
-            dynamic_template_data: %{
-              mentee_name: s.mentee.name,
-              parent_name: s.mentee.parent1_name,
-              mentor_name: s.mentor.name,
-              session_notes: s.notes
-            },
-            subject: s.email_subject
-          }
-        ],
-        from: %{
-          email: "dharik@transizion.com",
-          name: s.mentor.name
-        },
-        subject: "Strategy session: #{s.title}",
-        template_id: template_id,
-        mail_settings: %{
-          sandbox_mode: %{
-            enable: false
-          }
-        }
-      }
+      if s.mentee.parent1_email do
+        send_email(
+          s.mentee.parent1_email,
+          s.mentee.parent1_name,
+          s.mentee.name,
+          s.mentor.name,
+          s.notes,
+          s.title,
+          s.email_subject
+          "dharik@transizion.com",
+          s.mentor.name
+        )
+      end
 
-      {:ok, body_json} = Jason.encode(body)
-      IO.inspect(HTTPoison.post(url, body_json, headers))
+      if s.mentee.parent2_email do
+        end_email(
+          s.mentee.parent2_email,
+          s.mentee.parent2_name,
+          s.mentee.name,
+          s.mentor.name,
+          s.notes,
+          s.title,
+          s.email_subject
+          "dharik@transizion.com",
+          s.mentor.name
+        )
+      end
 
       StrategySession.email_sent_changeset(s) |> Repo.update
     end)
+  end
+
+  def send_email(
+    to_email,
+    to_name,
+    mentee_name,
+    mentor_name,
+    session_notes,
+    session_title,
+    subject,
+    from_email,
+    from_name,
+  ) do
+    body = %{
+      personalizations: [
+        %{
+          to: [
+            %{
+              email: to_email
+              name: to_name
+            }
+          ],
+          dynamic_template_data: %{
+            mentee_name: mentee_name,
+            parent_name: to_name,
+            mentor_name: mentor_name,
+            session_notes: session_notes
+          },
+          subject: subject
+        }
+      ],
+      from: %{
+        email: from_email,
+        name: from_name
+      },
+      subject: "Strategy session: #{session_title}",
+      template_id: @template_id
+    }
+
+    {:ok, body_json} = Jason.encode(body)
+    IO.inspect(HTTPoison.post(@sendgrid_url, body_json, headers))
   end
 end
