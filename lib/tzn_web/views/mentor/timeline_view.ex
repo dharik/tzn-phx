437effb33df36371a56f72b8ba7c9a6e2cab9ca8
Dharik Patel <dharik@magoosh.com>
2020-09-29 15:30:01 -0400

    Sort mentor timeline upcoming events in ascending order

--- lib/tzn_web/views/mentor/timeline_view.ex
+++ lib/tzn_web/views/mentor/timeline_view.ex
@@ -20,6 +20,13 @@ defmodule TznWeb.Mentor.TimelineView do
     |> Enum.group_by(fn e -> date_to_year_month_str(e.date) end)
   end
 
+  def sort_groups_asc(groups) do
+    groups
+    |> Enum.sort_by(fn {key, g} ->
+      g |> List.first() |> Map.get(:date) |> days_from_now
+    end)
+  end
+
   def date_to_year_month_str(date) do
     case Timex.format(date, "%B %Y", :strftime) do
       {:ok, formatted} -> formatted
@@ -36,14 +43,13 @@ defmodule TznWeb.Mentor.TimelineView do
     end
   end
 
-  def is_complete_for_mentee(event_marking, %{ id: mentee_id }) when is_nil(event_marking) do
+  def is_complete_for_mentee(event_marking, %{id: mentee_id}) when is_nil(event_marking) do
     false
   end
-  
-  def is_complete_for_mentee(event_marking, %{ id: mentee_id}) do
+
+  def is_complete_for_mentee(event_marking, %{id: mentee_id}) do
     event_marking.completed_for_mentees
     |> Enum.find(fn m -> m == mentee_id end)
     |> is_integer
   end
-
 end
