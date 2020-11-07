804a730d1a884f54727a46e2cc8d184a5e7590db
Dharik Patel <dharik@magoosh.com>
2020-11-07 10:58:50 -0500

    Fix host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :tzn, TznWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "collegerize.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
