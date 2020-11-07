48e38590b34f57dd649e8f379e4daa1e21601a4d
Dharik Patel <dharik@magoosh.com>
2020-11-07 11:40:27 -0500

    Use dynamic host

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :tzn, TznWeb.Endpoint,
   url: [host: "collegerize.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil]
 
 # Do not print debug messages in production
 config :logger, level: :info
