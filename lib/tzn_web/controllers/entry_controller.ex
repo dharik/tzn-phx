4c41f6dc63384d6c5b5a856672543c4ad27d196a
Dharik Patel <dharik@magoosh.com>
2020-12-23 19:03:04 -0500

    Admin UX improvements

--- lib/tzn_web/controllers/entry_controller.ex
+++ lib/tzn_web/controllers/entry_controller.ex
@@ -9,7 +9,7 @@ defmodule TznWeb.EntryController do
   def launch_app(conn, _) do
     cond do
       conn.assigns.current_admin ->
-        conn |> redirect(to: Routes.admin_user_path(conn, :index))
+        conn |> redirect(to: Routes.admin_mentor_path(conn, :index))
       conn.assigns.current_mentor ->
         conn |> redirect(to: Routes.mentor_mentee_path(conn, :index))
       true ->
