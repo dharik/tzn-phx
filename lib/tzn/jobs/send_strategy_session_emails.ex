13ba14a681756e30d995fd0f6f7e89f66d459e59
Dharik Patel <dharik@magoosh.com>
2021-01-26 20:06:11 -0500

    Help parents keep up with hours left more easily

--- lib/tzn/jobs/send_strategy_session_emails.ex
+++ lib/tzn/jobs/send_strategy_session_emails.ex
@@ -8,19 +8,25 @@ defmodule Tzn.Jobs.SendStrategySessionEmails do
   @sendgrid_url "https://api.sendgrid.com/v3/mail/send"
 
   import Logger
-  
+
   def run do
-    Logger.info "Running strategy session emails job"
+    Logger.info("Running strategy session emails job")
 
     sessions =
       Repo.all(
         from s in StrategySession,
-          where: s.emailed == false and s.published ==  true
+          where: s.emailed == false and s.published == true
       )
-      |> Repo.preload([:mentee, :mentor])
+      |> Repo.preload([:mentor, mentee: [:hour_counts]])
 
     sessions
     |> Enum.map(fn s ->
+      hours_left =
+        s.mentee.hour_counts.hours_purchased
+        |> Decimal.sub(s.mentee.hour_counts.hours_used)
+        |> Decimal.round(1)
+        |> Decimal.to_float()
+
       if s.mentee.parent1_email do
         send_email(
           s.mentee.parent1_email,
@@ -31,7 +37,8 @@ defmodule Tzn.Jobs.SendStrategySessionEmails do
           s.email_subject,
           s.mentor.email,
           s.mentor.name,
-          s.mentee.email
+          s.mentee.email,
+          hours_left
         )
       end
 
@@ -45,51 +52,54 @@ defmodule Tzn.Jobs.SendStrategySessionEmails do
           s.email_subject,
           s.mentor.email,
           s.mentor.name,
-          s.mentee.email
+          s.mentee.email,
+          hours_left
         )
       end
 
-      StrategySession.email_sent_changeset(s) |> Repo.update
+      StrategySession.email_sent_changeset(s) |> Repo.update()
     end)
   end
 
   def send_email(
-    to_email,
-    to_name,
-    mentee_name,
-    mentor_name,
-    session_notes,
-    subject,
-    reply_to_email,
-    from_name,
-    mentee_email
-  ) do
+        to_email,
+        to_name,
+        mentee_name,
+        mentor_name,
+        session_notes,
+        subject,
+        reply_to_email,
+        from_name,
+        mentee_email,
+        hours_left
+      ) do
     headers = [
-      Authorization: Application.get_env(:tzn, :sendgrid_auth),
+      Application.get_env(:tzn, :sendgrid_auth),
       "Content-Type": "application/json"
     ]
 
-    Logger.info "Sending strategy session email to #{to_email}"
+    Logger.info("Sending strategy session email to #{to_email}")
 
-    cc = if mentee_email do
-       [
-            %{
-              email: reply_to_email,
-              name: from_name
-            },
-            %{
-                email: mentee_email,
-                name: mentee_name
-              }
-          ]
-    else
+    cc =
+      if mentee_email do
         [
-            %{
-              email: reply_to_email,
-              name: from_name
-            }
-          ]
-    end
+          %{
+            email: reply_to_email,
+            name: from_name
+          },
+          %{
+            email: mentee_email,
+            name: mentee_name
+          }
+        ]
+      else
+        [
+          %{
+            email: reply_to_email,
+            name: from_name
+          }
+        ]
+      end
 
     body = %{
       personalizations: [
@@ -105,7 +115,8 @@ defmodule Tzn.Jobs.SendStrategySessionEmails do
             mentee_name: mentee_name,
             parent_name: to_name,
             mentor_name: mentor_name,
-            session_notes: session_notes
+            session_notes: session_notes,
+            hours_left: hours_left
           },
           subject: subject
         }
