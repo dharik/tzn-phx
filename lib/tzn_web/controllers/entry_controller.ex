1fa118664743b71d954d1f3c1a58e96e86f6e57e
Dharik Patel <dharik@magoosh.com>
2021-04-25 11:14:28 -0400

    Minor improvements

--- lib/tzn_web/controllers/entry_controller.ex
+++ lib/tzn_web/controllers/entry_controller.ex
@@ -8,6 +8,8 @@ defmodule TznWeb.EntryController do
 
   def launch_app(conn, _) do
     cond do
+      conn.assigns.current_admin && conn.assigns.current_mentor ->
+        render(conn, "choose_profile.html")
       conn.assigns.current_admin ->
         conn |> redirect(to: Routes.admin_mentor_path(conn, :index))
       conn.assigns.current_mentor ->
