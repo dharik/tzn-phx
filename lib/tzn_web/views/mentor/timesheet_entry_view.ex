85c27e709df8c3b130b47378565c0ddfea5ada5d
Dharik Patel <dharik@magoosh.com>
2021-09-12 15:58:22 +0000

    Mentor pay calculation wasn't using per-mentee rate override

--- lib/tzn_web/views/mentor/timesheet_entry_view.ex
+++ lib/tzn_web/views/mentor/timesheet_entry_view.ex
@@ -2,7 +2,7 @@ defmodule TznWeb.Mentor.TimesheetEntryView do
   use TznWeb, :view
 
   def format_date(date) do
-    case Timex.format(date, "%b %d %l:%M %p", :strftime) do
+    case Timex.format(date, "%b %d %Y %l:%M %p", :strftime) do
       {:ok, formatted} -> formatted
       {:error, _} -> date
       _ -> "N/A"
