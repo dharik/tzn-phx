ade64c74b197c4616375f6dd042ef70184b8ce72
Dharik Patel <dharik92@gmail.com>
2021-12-27 16:11:08 +0000

    Test email config again

--- config/releases.exs
+++ config/releases.exs
@@ -9,4 +9,4 @@ config :tzn, sendgrid_auth: System.get_env("SENDGRID_AUTH_HEADER")
 
 config :tzn, Tzn.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
-  api_key: System.get_env("SENDGRID_AUTH_HEADER")
+  api_key: System.get_env("SENDGRID_API_KEY")
