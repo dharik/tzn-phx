36dbf2fa3d2fabf1ffe5f0bdcc294a9359b1b384
Dharik Patel <dharik92@gmail.com>
2022-01-05 01:45:17 +0000

    Actually email the parents

--- lib/tzn/emails/questionnaire.ex
+++ lib/tzn/emails/questionnaire.ex
@@ -4,11 +4,10 @@ defmodule Tzn.Emails.Questionnaire do
   def welcome(body, mentee_name, parent_email, mentor_name, mentor_email) do
     new()
     |> from({"Transizion", "mentors@transizion.com"})
-    |> to("mentors@transizion.com")
-    # |> to(parent_email)
-    # |> cc({mentor_name, mentor_email})
+    |> to(parent_email)
+    |> cc({mentor_name, mentor_email})
     |> reply_to({mentor_name, mentor_email})
-    # |> bcc("mentors@transizion.com")
+    |> bcc("mentors@transizion.com")
     |> subject("#{mentee_name}'s College List")
     |> html_body(body)
   end
