fd0d7895955a64645d7fde9ab61c1ddb58d5b032
Dharik Patel <dharik92@gmail.com>
2022-03-13 15:21:59 +0000

    Switch up admin layout

--- assets/js/matching.jsx
+++ assets/js/matching.jsx
@@ -1,8 +1,8 @@
-import { h, render, Component, Fragment } from "preact";
-import { useState } from "preact/hooks";
-import "preact/devtools";
+import { h, render, Component, Fragment } from 'preact';
+import { useState } from 'preact/hooks';
+import 'preact/devtools';
 
-export default ({mentors, options}) => {
+export default ({ mentors, options }) => {
   const slugToLabel = {};
 
   if (options) {
@@ -73,13 +73,7 @@ export default ({mentors, options}) => {
         disabilityScore,
         socialFactorScore,
         hobbiesScore,
-        totalScore:
-          careerScore +
-          tierScore +
-          genderScore +
-          disabilityScore +
-          socialFactorScore +
-          hobbiesScore,
+        totalScore: careerScore + tierScore + genderScore + disabilityScore + socialFactorScore + hobbiesScore,
       };
     })
     .sort((a, b) => b.totalScore - a.totalScore);
@@ -114,225 +108,213 @@ export default ({mentors, options}) => {
   );
   const form = (
     <>
-      <strong>Prospective Major</strong>
-      <br />
-      {options.careers.map(([name, value]) => (
-        <label key={value}>
-          <input
-            type="checkbox"
-            checked={careers.has(value)}
-            onClick={() => setCareers(toggleSetItem(careers, value))}
-          />{" "}
-          {name}
+      <div className="background-light-100 padding-s">
+        <strong>Prospective Major</strong>
+      </div>
+      <div className="margin-vertical-s padding-horizontal-m">
+        {options.careers.map(([name, value]) => (
+          <label key={value} className="control checkbox">
+            <input
+              type="checkbox"
+              checked={careers.has(value)}
+              onClick={() => setCareers(toggleSetItem(careers, value))}
+            />
+            <span class="control-indicator"></span>
+            <span class="control-label">{name}</span>
+          </label>
+        ))}
+      </div>
+      <div className="space-m"></div>
+
+      <div className="background-light-100 padding-s">
+        <strong>Schools Tier</strong>
+      </div>
+      <div className="margin-vertical-s padding-horizontal-m">
+        {options.school_tiers.map(([name, value]) => (
+          <label key={value} className="control checkbox">
+            <input
+              type="checkbox"
+              checked={schoolTiers.has(value)}
+              onClick={() => setSchoolTiers(toggleSetItem(schoolTiers, value))}
+            />
+            <span class="control-indicator"></span>
+            <span class="control-label">{name}</span>
+          </label>
+        ))}
+      </div>
+      <div className="space-m"></div>
+
+      <div className="background-light-100 padding-s">
+        <strong>Gender preference</strong>
+      </div>
+      <div className="margin-vertical-s padding-horizontal-m">
+        <label class="control radio">
+          <input type="radio" checked={genderPreference === 'male'} onClick={() => setGenderPreference('male')} />
+          <span class="control-indicator"></span>
+          <span class="control-label">Male</span>
         </label>
-      ))}
-      <div className="space-m"></div>
-      <strong>What tier of schools does the student want to apply to?</strong>
-      <br />
-
-      {options.school_tiers.map(([name, value]) => (
-        <label key={value}>
-          <input
-            type="checkbox"
-            checked={schoolTiers.has(value)}
-            onClick={() => setSchoolTiers(toggleSetItem(schoolTiers, value))}
-          />{" "}
-          {name}
+        <label class="control radio">
+          <input type="radio" checked={genderPreference === 'female'} onClick={() => setGenderPreference('female')} />{' '}
+          <span class="control-indicator"></span>
+          <span class="control-label">Female</span>
         </label>
-      ))}
-
-      <div className="space-m"></div>
-      <strong>Gender preference</strong>
-      <br />
-      <label>
-        <input
-          type="radio"
-          checked={genderPreference === "male"}
-          onClick={() => setGenderPreference("male")}
-        />{" "}
-        Male
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={genderPreference === "female"}
-          onClick={() => setGenderPreference("female")}
-        />{" "}
-        Female
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={genderPreference === "non_binary"}
-          onClick={() => setGenderPreference("non_binary")}
-        />{" "}
-        Non-binary
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={genderPreference === null}
-          onClick={() => setGenderPreference(null)}
-        />{" "}
-        No preference
-      </label>
-
-      <div className="space-m"></div>
-
-      <strong>Does student have a learning disability?</strong>
-      <br />
-      <label>
-        <input
-          type="radio"
-          checked={learningDisability}
-          onClick={() => setLearningDisability(true)}
-        />{" "}
-        Yes
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={!learningDisability}
-          onClick={() => setLearningDisability(false)}
-        />{" "}
-        No
-      </label>
-
-      <div className="space-m"></div>
-      <strong>Social Factor Preference</strong>
-      <br />
-      <label>
-        <input
-          type="radio"
-          checked={socialFactor === 1}
-          onClick={() => setSocialFactor(1)}
-        />{" "}
-        1
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={socialFactor === 2}
-          onClick={() => setSocialFactor(2)}
-        />{" "}
-        2
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={socialFactor === 3}
-          onClick={() => setSocialFactor(3)}
-        />{" "}
-        3
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={socialFactor === 4}
-          onClick={() => setSocialFactor(4)}
-        />{" "}
-        4
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={socialFactor === 5}
-          onClick={() => setSocialFactor(5)}
-        />{" "}
-        5
-      </label>
-      <label>
-        <input
-          type="radio"
-          checked={socialFactor === null}
-          onClick={() => setSocialFactor(null)}
-        />{" "}
-        None
-      </label>
-
-      <div className="space-m"></div>
-      <strong>Hobbies</strong>
-      <br />
-      {options.hobbies.map(([name, value]) => (
-        <label key={value}>
+        <label class="control radio">
           <input
-            type="checkbox"
-            checked={hobbies.has(value)}
-            onClick={() => setHobbies(toggleSetItem(hobbies, value))}
-          />{" "}
-          {name}
+            type="radio"
+            checked={genderPreference === 'non_binary'}
+            onClick={() => setGenderPreference('non_binary')}
+          />{' '}
+          <span class="control-indicator"></span>
+          <span class="control-label">Non-binary</span>
         </label>
-      ))}
+        <label class="control radio">
+          <input type="radio" checked={genderPreference === null} onClick={() => setGenderPreference(null)} />
+          <span class="control-indicator"></span>
+          <span class="control-label">No preference</span>
+        </label>
+      </div>
+      <div className="space-m"></div>
+
+      <div className="background-light-100 padding-s">
+        <strong>Learning Disability</strong>
+      </div>
+      <div className="margin-vertical-s padding-horizontal-m">
+        <label class="control radio">
+          <input type="radio" checked={learningDisability} onClick={() => setLearningDisability(true)} />
+          <span class="control-indicator"></span>
+          <span class="control-label">Yes</span>
+        </label>
+        <label class="control radio">
+          <input type="radio" checked={!learningDisability} onClick={() => setLearningDisability(false)} />
+          <span class="control-indicator"></span>
+          <span class="control-label">No</span>
+        </label>
+      </div>
+      <div className="space-m"></div>
+
+      <div className="background-light-100 padding-s">
+        <strong>Social Factor</strong>
+      </div>
+      <div className="margin-vertical-s padding-horizontal-m">
+        <label class="control radio">
+          <input type="radio" checked={socialFactor === 1} onClick={() => setSocialFactor(1)} />
+          <span class="control-indicator"></span>
+          <span class="control-label">1</span>
+        </label>
+        <label class="control radio">
+          <input type="radio" checked={socialFactor === 2} onClick={() => setSocialFactor(2)} />
+          <span class="control-indicator"></span>
+          <span class="control-label">2</span>
+        </label>
+        <label class="control radio">
+          <input type="radio" checked={socialFactor === 3} onClick={() => setSocialFactor(3)} />
+          <span class="control-indicator"></span>
+          <span class="control-label">3</span>
+        </label>
+        <label class="control radio">
+          <input type="radio" checked={socialFactor === 4} onClick={() => setSocialFactor(4)} />
+          <span class="control-indicator"></span>
+          <span class="control-label">4</span>
+        </label>
+        <label class="control radio">
+          <input type="radio" checked={socialFactor === 5} onClick={() => setSocialFactor(5)} />
+          <span class="control-indicator"></span>
+          <span class="control-label">5</span>
+        </label>
+      </div>
+      <div className="space-m"></div>
+
+      <div className="background-light-100 padding-s">
+        <strong>Hobbies</strong>
+      </div>
+      <div className="margin-vertical-s padding-horizontal-m">
+        {options.hobbies.map(([name, value]) => (
+          <label key={value} className="control checkbox">
+            <input
+              type="checkbox"
+              checked={hobbies.has(value)}
+              onClick={() => setHobbies(toggleSetItem(hobbies, value))}
+            />
+            <span class="control-indicator"></span>
+            <span class="control-label">{name}</span>
+          </label>
+        ))}
+      </div>
     </>
   );
   return (
     <div className="flex">
-      <div className="flex-1">{form}</div>
-      <div className="flex-1">
+      <div className="flex-1 matching-result">
         {scoredMentors.slice(0, 5).map((mentor, idx) => (
           <div
-            className="box-shadow-s border-radius margin-vertical-s padding-s transition-fast"
+            className="border border-radius margin-vertical-s padding-s transition-fast flex flex-column align-items-center text-align-center"
             key={idx}
           >
-            <a href={`/admin/mentors/${mentor.id}`}>
-              <h1 className="all-caps">{mentor.name}</h1>
-            </a>
-            <div className="flex margin-vertical-xs">
+            <div>
               {mentor.photo_url ? (
                 <img
                   src={mentor.photo_url}
-                  width="120"
-                  height="120"
-                  className="margin-right-s"
+                  className=""
+                  style="height: 200px; width: 200px; border-radius: 50%; object-fit: cover;"
                 />
               ) : (
                 <svg
-                  fill="lavender"
+                  style="border-radius: 50%;"
+                  class="background-light-50 fill-dark-50"
                   viewBox="0 0 24 24"
-                  width="120"
-                  height="120"
+                  width="200"
+                  height="200"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
                 </svg>
               )}
-
-              <div className="flex-1">
-                {mentor.careerScore > 0 && (
-                  <p>
-                    Similar career interests:{" "}
-                    {mentor.career_interests
-                      .filter((career) => careers.has(career))
-                      .map((career) => slugToLabel[career] || career)
-                      .join(", ")}
-                  </p>
-                )}
-                {mentor.hobbiesScore > 0 && (
-                  <p>
-                    Similar hobbies:{" "}
-                    {mentor.hobbies
-                      .filter((hobby) => hobbies.has(hobby))
-                      .map((hobby) => slugToLabel[hobby] || hobby)
-                      .join(", ")}
-                  </p>
-                )}
-                {mentor.disabilityScore > 0 && (
-                  <p>Experience with learning disabilities</p>
-                )}
-                {mentor.tierScore > 0 && (
-                  <p>
-                    Experience with{" "}
-                    {mentor.school_tiers
-                      .map((tier) => slugToLabel[tier])
-                      .join(", ")}{" "}
-                    schools
-                  </p>
-                )}
-              </div>
             </div>
+
+            <p className="lead-xs">{mentor.name}</p>
+
+            {mentor.careerScore > 0 && (
+              <p>
+                <span class="text-transform-uppercase color-grey font-size-s">Similar career interests</span>
+                <br />
+                {mentor.career_interests
+                  .filter((career) => careers.has(career))
+                  .map((career) => slugToLabel[career] || career)
+                  .join(', ')}
+              </p>
+            )}
+
+            {mentor.hobbiesScore > 0 && (
+              <p>
+                <span class="text-transform-uppercase color-grey font-size-s">Similar hobbies:</span>
+                <br />
+                {mentor.hobbies
+                  .filter((hobby) => hobbies.has(hobby))
+                  .map((hobby) => slugToLabel[hobby] || hobby)
+                  .join(', ')}
+              </p>
+            )}
+            {mentor.disabilityScore > 0 && (
+              <p class="text-transform-uppercase color-grey font-size-s">Experience with learning disabilities</p>
+            )}
+            {mentor.tierScore > 0 && (
+              <p class="text-transform-uppercase color-grey font-size-s">
+                Experience with {mentor.school_tiers.map((tier) => slugToLabel[tier]).join(', ')} schools
+              </p>
+            )}
+
+            <a
+              href={`/admin/mentors/${mentor.id}`}
+              className="text-decoration-none color-primary-500 font-weight-medium"
+            >
+              View profile
+            </a>
           </div>
         ))}
         {/* {scoredMentorTable} */}
+      </div>
+      <div className="flex-1 padding-horizontal-m" style="max-width: 40rem">
+        {form}
       </div>
     </div>
   );
