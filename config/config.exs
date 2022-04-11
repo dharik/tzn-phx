618e7923d59935167b1afa85ef12e60b6112f1b8
Dharik Patel <dharik92@gmail.com>
2022-04-11 18:54:16 +0000

    No longer used

--- config/config.exs
+++ config/config.exs
@@ -42,7 +42,6 @@ config :tzn, :pow,
 
 config :tzn, Tzn.Scheduler,
 jobs: [
-  {"0 */3 * * *",         {Tzn.Jobs.SendStrategySessionEmails, :run, []}},
   {"0 */1 * * *",         {Tzn.Jobs.CleanupMenteeChanges, :run, []}},
   {"0 0 */1 * *",         {Tzn.Jobs.ParentUpdateEmails, :todo_list_changed, []}},
   {"0 0 */15 * *",         {Tzn.Jobs.ParentUpdateEmails, :biweekly_minimum, []}}
--- config/releases.exs
+++ config/releases.exs
@@ -1,7 +1,5 @@
 import Config
 
-config :tzn, sendgrid_auth: System.get_env("SENDGRID_AUTH_HEADER")
-
 config :tzn, Tzn.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
   api_key: System.get_env("SENDGRID_API_KEY")
