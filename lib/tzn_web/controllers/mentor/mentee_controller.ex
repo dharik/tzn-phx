c1a51cfe71b6087f38257711d679d0fea3691365
Dharik Patel <dharik92@gmail.com>
2022-04-14 18:18:07 +0000

    Unused

--- lib/tzn_web/controllers/mentor/mentee_controller.ex
+++ lib/tzn_web/controllers/mentor/mentee_controller.ex
@@ -9,23 +9,6 @@ defmodule TznWeb.Mentor.MenteeController do
     render(conn, "index.html", mentees: conn.assigns.mentees)
   end
 
-  def new(conn, _params) do
-    changeset = Transizion.change_mentee(%Mentee{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"mentee" => mentee_params}) do
-    case Transizion.create_mentee(mentee_params) do
-      {:ok, mentee} ->
-        conn
-        |> put_flash(:info, "Mentee created successfully.")
-        |> redirect(to: Routes.mentor_mentee_path(conn, :show, mentee))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
   def show(conn, %{"id" => id}) do
     mentee =
       Transizion.get_mentee!(id)
@@ -72,7 +55,6 @@ defmodule TznWeb.Mentor.MenteeController do
     case Transizion.update_mentee(mentee, mentee_params, conn.assigns[:current_user]) do
       {:ok, mentee} ->
         conn
-        |> put_flash(:info, "Mentee updated successfully.")
         |> redirect(to: Routes.mentor_mentee_path(conn, :show, mentee))
 
       {:error, %Ecto.Changeset{} = changeset} ->
@@ -104,14 +86,5 @@ defmodule TznWeb.Mentor.MenteeController do
       {:ok, mentee} -> json(conn, %{value: mentee.mentor_todo_notes})
       {:error, changeset} -> conn |> send_resp(403, "Error" <> changeset.errors)
     end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    mentee = Transizion.get_mentee!(id)
-    {:ok, _mentee} = Transizion.delete_mentee(mentee)
-
-    conn
-    |> put_flash(:info, "Mentee deleted successfully.")
-    |> redirect(to: Routes.mentor_mentee_path(conn, :index))
   end
 end
