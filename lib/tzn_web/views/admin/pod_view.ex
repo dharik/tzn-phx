550cee7b0b940c40ab46f220266ee0aa719d6cdc
Dharik Patel <dharik92@gmail.com>
2022-08-09 20:50:51 +0000

    Admin pod view of todos needed sorting

--- lib/tzn_web/views/admin/pod_view.ex
+++ lib/tzn_web/views/admin/pod_view.ex
@@ -8,4 +8,14 @@ defmodule TznWeb.Admin.PodView do
       _ -> "N/A"
     end
   end
+
+  def sort_todos(todos) do
+    Enum.sort_by(todos, fn todo ->
+      cond do
+        todo.deleted_at -> {-1, todo.inserted_at |> Timex.to_unix()}
+        todo.completed -> {0, todo.inserted_at |> Timex.to_unix()}
+        true -> {1, todo.inserted_at |> Timex.to_unix()}
+      end
+    end) |> Enum.reverse()
+  end
 end
