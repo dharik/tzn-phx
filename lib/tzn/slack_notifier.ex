b5fbfa0ba564928e9cebc7c8c507a6e2fbb4e6af
Dharik Patel <dharik92@gmail.com>
2022-09-21 16:54:13 +0000

    Don't need this right now

--- lib/tzn/slack_notifier.ex
+++ lib/tzn/slack_notifier.ex
@@ -2,15 +2,11 @@ defmodule Tzn.SlackNotifier do
   require Logger
 
   def send_to_coding(message) do
-    if Mix.env() in [:dev, :test] do
-      Logger.info("Message to slack: " <> message)
-    else
-      HTTPoison.post(
-        "https://hooks.slack.com/services/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",
-        Jason.encode!(%{
-          text: message
-        })
-      )
-    end
+    HTTPoison.post(
+      "https://hooks.slack.com/services/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",
+      Jason.encode!(%{
+        text: message
+      })
+    )
   end
 end
