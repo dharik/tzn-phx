9021b2a985ae3d15c9bdb16a82829ab79f4ffea2
Dharik Patel <dharik92@gmail.com>
2022-09-21 16:58:44 +0000

    Typeform uses a different key based on the data type

--- lib/tzn_web/controllers/typeform_webhook_controller.ex
+++ lib/tzn_web/controllers/typeform_webhook_controller.ex
@@ -4,20 +4,20 @@ defmodule TznWeb.TypeformWebhookController do
 
   def handle(conn, %{"form_response" => %{"answers" => answers}}) do
     mentee_name = get_answer_by_field_id(answers, "e1be2b5d042d25ef")
-    mentee_phone = get_answer_by_field_id(answers, "16e1f32b51b6ab87")
-    mentee_email = get_answer_by_field_id(answers, "bb066f78-e17e-4ed3-bc01-e2e08db4fad5")
+    mentee_phone = get_answer_by_field_id(answers, "16e1f32b51b6ab87", "phone_number")
+    mentee_email = get_answer_by_field_id(answers, "bb066f78-e17e-4ed3-bc01-e2e08db4fad5", "email")
 
     school_goals = get_answer_by_field_id(answers, "6cae8e678b4c92da")
 
     parent1_first_name = get_answer_by_field_id(answers, "c778a0f5-a789-401b-b7b8-5962ca076ab2")
     parent1_last_name = get_answer_by_field_id(answers, "18c6ceae-1881-43fc-b5f1-9d85d3f63cb5")
-    parent1_phone = get_answer_by_field_id(answers, "d905a713-ce18-4ba9-8542-74b33320589d")
-    parent1_email = get_answer_by_field_id(answers, "38f065e6-6f63-47c1-aa24-462176de4862")
+    parent1_phone = get_answer_by_field_id(answers, "d905a713-ce18-4ba9-8542-74b33320589d", "phone_number")
+    parent1_email = get_answer_by_field_id(answers, "38f065e6-6f63-47c1-aa24-462176de4862", "email")
 
     parent2_first_name = get_answer_by_field_id(answers, "e1c9b155-b25b-484b-b41e-53c55a1da318")
     parent2_last_name = get_answer_by_field_id(answers, "a89f0596-73b9-4613-a38d-c22cc2a3f3c8")
-    parent2_phone = get_answer_by_field_id(answers, "151597ee-b00d-4634-b0c4-287ba49d06cd")
-    parent2_email = get_answer_by_field_id(answers, "ce462e37-19ec-45a9-ab18-55f47c365a9d")
+    parent2_phone = get_answer_by_field_id(answers, "151597ee-b00d-4634-b0c4-287ba49d06cd", "phone_number")
+    parent2_email = get_answer_by_field_id(answers, "ce462e37-19ec-45a9-ab18-55f47c365a9d", "email")
 
     formatted_data = %{
       mentee: %{
@@ -43,7 +43,7 @@ defmodule TznWeb.TypeformWebhookController do
     conn |> text("ok")
   end
 
-  def get_answer_by_field_id(answers, field_id) when is_list(answers) and is_binary(field_id) do
-    Enum.find(answers, %{}, fn a -> a["field"]["ref"] == field_id end) |> Map.get("text", "")
+  def get_answer_by_field_id(answers, field_id, field_type \\ "text") when is_list(answers) and is_binary(field_id) do
+    Enum.find(answers, %{}, fn a -> a["field"]["ref"] == field_id end) |> Map.get(field_type, "")
   end
 end
